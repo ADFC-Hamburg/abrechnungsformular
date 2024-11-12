d875d01bc6f60b5eca84c223bcf32f12c2d980f9
Patrick Lübcke <patrickluebcke@gmail.com>
2024-11-12 15:05:40 +0100

    added docstrings

--- abrechnungsformular.py
+++ abrechnungsformular.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+"""
+Startet einen Flask-Server, welcher eine Webseite anbietet, auf der
+Benutzer ein Formular für eine Aktivenabrechnung ausfüllen und
+anschließend eine fertige Abrechnung im PDF-Format herunterladen
+können.
+"""
+
+from flask import Flask, render_template, request, abort
 
 from app import aktive
 
@@ -9,6 +16,9 @@ server = Flask(__name__)
 @server.route('/index')
 @server.route('/')
 def index():
+    """
+    Zeigt das Formular zur Erstellung einer Aktivenabrechnung an
+    """
     return render_template('form_aktive.html')
 
 if __name__ == '__main__':
