949b253d39b453c74306169ecabed1c03cbcbc5b
Patrick Lübcke <patrickluebcke@gmail.com>
2025-01-13 11:16:43 +0100

    Provide contact information as dictionary

--- Dockerfile
+++ Dockerfile
@@ -32,7 +32,7 @@ USER appuser
 COPY app/ /abrechnungsformular/app/
 COPY static/ /abrechnungsformular/static/
 COPY templates/ /abrechnungsformular/templates/
-COPY abrechnungsformular.py VERSION /abrechnungsformular/
+COPY abrechnungsformular.py VERSION CONTACT.json /abrechnungsformular/
 
 # Expose port 8000 for the Flask application
 EXPOSE 8000
--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
+import json
+
+def _fetch_json(path:str):
+    with open(path) as f:
+        data = json.load(f)
+        return data
+
 def _fetch_version(path:str) -> str:
     with open(path) as f:
         version = f.readline()
@@ -5,3 +12,5 @@ def _fetch_version(path:str) -> str:
 
 #: Die Versionsnummer dieser App.
 VERSION = _fetch_version('VERSION')
+#: Die Kontaktdaten des Vereins.
+CONTACT = _fetch_json('CONTACT.json')
