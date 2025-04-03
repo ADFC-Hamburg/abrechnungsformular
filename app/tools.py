49f6a67dcde1f0715e7a5c5fbd5f3c3c08d11a13
Patrick Lübcke <patrickluebcke@gmail.com>
2025-04-03 12:10:29 +0200

    Reise template compatibility and paths

--- app/tools.py
+++ app/tools.py
@@ -3,8 +3,8 @@ Sammelmodul für diverse Funktionen, die im Ausgabeprogramm
 der Aktivenabrechnung zum Einsatz kommen.
 """
 
-from babel.numbers import format_currency
-from babel.dates import format_date
+from babel.numbers import format_currency, format_decimal
+from babel.dates import format_date, format_time
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from . import CONTACT, PATHS, VERSION
@@ -46,7 +46,7 @@ def euro(value = 0, empty = False) -> str:
     Gibt eine Zahl als Eurobetrag zurück.
     Ist empty True, wird statt 0,00 € ein leerer String zurückgegeben.
     """
-    if empty and value == 0:
+    if empty and not value:
         return ""
     return format_currency(value,"EUR",locale="de_DE")
 
@@ -72,4 +72,5 @@ def write_list_de(list:list[str]) -> str:
 
 pdf_environment = Environment(loader=FileSystemLoader(PATHS.PDF_TEMPLATE_FOLDER))
 pdf_environment.globals.update(address=CONTACT,checkbox=checkbox,euro=euro,
-                               format_date=format_date,version=VERSION)
+                               format_date=format_date,format_decimal=format_decimal,
+                               format_time=format_time,version=VERSION)
