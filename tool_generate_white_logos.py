b33e2b724a01644e3d2e7f6e004f5faec5de0806
Patrick Lübcke <patrickluebcke@gmail.com>
2025-05-08 11:50:59 +0200

    Options for overwrite, verbosity

--- tool_generate_white_logos.py
+++ tool_generate_white_logos.py
@@ -26,6 +26,8 @@ DESCRIPTION = "Dieses Script erstellt weiße Varianten transparenter" \
 
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 parser.add_argument('file',nargs='+',help='Die zu bearbeitende(n) SVG-Datei(en)')
+parser.add_argument('-o','--overwrite',action='store_true',help='Überschreibe existierende Dateien')
+parser.add_argument('-v','--verbose',action='store_true',help='Gib ausführlichere Logs aus')
 if len(sys.argv)==1:
     parser.print_help(sys.stderr)
     sys.exit(1)
@@ -35,11 +37,19 @@ args=parser.parse_args()
 from pathlib import Path
 from re import sub
 
-def semiwhite(svg:str):
+def semiwhite(svg:str) -> str:
     return sub("#[0-7][0-9a-fA-F][0-7][0-9a-fA-F]{3}","#ffffff",svg)
 
-def white(svg:str):
+def white(svg:str) -> str:
     return sub("#[0-9a-fA-F]{6}","#ffffff",svg)
+
+def create_file(path:Path,svg:str):
+    if not path.exists() or args.overwrite:
+        path.write_text(svg)
+        if args.verbose:
+            print(f'Erstellt: {str(path)}')
+    elif args.verbose:
+        print(f'Die Datei {str(path)} existiert bereits.')
 
 for file in args.file:
     path = Path(file)
@@ -55,7 +65,5 @@ for file in args.file:
             svg_white = white(svg_semi)
             path_semi = path.with_stem(path.stem+'-semiwhite')
             path_white = path.with_stem(path.stem+'-white')
-            if not path_semi.exists():
-                path_semi.write_text(svg_semi)
-            if not path_white.exists():
-                path_white.write_text(svg_white)
+            create_file(path_semi,svg_semi)
+            create_file(path_white,svg_white)
